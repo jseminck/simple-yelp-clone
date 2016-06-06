659af13878a323f86a4e3cb342938e2eb78cadaa
Joachim Seminck <joachim.seminck@zeroturnaround.com>
2016-06-06 21:25:54 +0300

    Search for nearby cafes near a certain location

--- src/views/main/Container.js
+++ src/views/main/Container.js
@@ -1,14 +1,55 @@
 import React from 'react';
 import Map, {GoogleApiWrapper} from 'google-maps-react';
+import {searchNearby} from 'utils/google/apiHelpers';
 
 export class Container extends React.Component {
+    static propTypes = {
+        google: React.PropTypes.object
+    };
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            places: [],
+            pagination: null
+        }
+    }
+
     render() {
         return (
             <div>
                 Hello from the container
-                <Map google={this.props.google} />
+                <Map
+                    google={this.props.google}
+                    onReady={::this.onReady}
+                    visible={false}
+                >
+                    {this.state.places.map(place => {
+                        return (<div key={place.id}>{place.name}</div>)
+                    })}
+                </Map>
             </div>
         );
+    }
+
+    onReady(mapProps, map) {
+        const opts = {
+            location: map.center,
+            radius: '500',
+            types: ['cafe']
+        }
+
+        searchNearby(this.props.google, map, opts)
+            .then((results, pagination) => {
+                this.setState({
+                    places: results,
+                    pagination
+                });
+            })
+            .catch((status, result) => {
+                // There was an error
+            });
     }
 }
 
